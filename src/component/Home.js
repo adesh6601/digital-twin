cd026a93032c3d6c619d213cc0ee4d4834537942
Kunal <kunal.more@cctech.co.in>
2024-03-12 18:19:12 +0530

    titlebar-and-sidebar-updated

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,32 +1,62 @@
 import { Grid, Typography } from "@mui/material";
-import makeStyles from '@mui/styles/makeStyles'
+import makeStyles from "@mui/styles/makeStyles";
 import React from "react";
 import SideBar from "./SideBar";
 import Graphs from "./Graphs";
 import ActuatorInfo from "./ActuatorInfo";
 
 const useStyles = makeStyles({
-  titleBar : {
-    backgroundColor : "skyblue", height : '7vh', fontSize : '1rem'
-  }
+  titleBarCSS: {
+    backgroundColor: "#9eb9e9",
+    height: "8vh",
+  },
+  SideBarCSS: {
+    backgroundColor: "#ece9d9",
+    height: "92vh",
+  },
+  viewerAreaCSS: {
+    backgroundColor: "#ece9d9",
+    height: "92vh",
+  },
+  graphsAreaCSS: {
+    backgroundColor: "#ece9d9",
+    height: "92vh",
+  },
 });
 
 function Home() {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <div>
-      <Grid item xs={12} className={classes.titleBar}>
-          <Typography> Actuator Digital Twin </Typography>
+      <Grid item xs={12} className={classes.titleBarCSS}>
+        <Typography
+          style={{
+            fontSize: "35px",
+            fontWeight: "bold",
+            textAlign: "left",
+            marginLeft: "20px",
+            color: "#f4fcff",
+          }}
+        >
+          Actuator Digital Twin
+        </Typography>
       </Grid>
-      <Grid container item xs = {12}>
-        <Grid item xs = {1} style = {{backgroundColor : "blue", height : "93vh"}}>
-          <SideBar/>
+      <Grid
+        container
+        item
+        xs={12}
+      >
+        <Grid item xs={1} className={classes.SideBarCSS} style={{
+          border: "0.5px solid black", // Make the border visible
+        }}>
+          <SideBar />
         </Grid>
-        <Grid item xs = {5.5} style = {{backgroundColor : "black"}}>
+        <Grid item xs={5.5} className={classes.viewerAreaCSS}>
           Viewer area
+          {/* <ActuatorInfo/> */}
         </Grid>
-        <Grid item xs = {5.5} style = {{backgroundColor : "green"}}>
-          <Graphs/>
+        <Grid item xs={5.5} className={classes.graphsAreaCSS}>
+          <Graphs />
         </Grid>
       </Grid>
     </div>
--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -1,20 +1,28 @@
 import { Grid } from "@mui/material";
+import makeStyles from '@mui/styles/makeStyles'
 import React from "react";
 import FolderIcon from "@mui/icons-material/Folder";
 import TuneIcon from "@mui/icons-material/Tune";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 
+const useStyles = makeStyles({
+  folderIconCSS : {
+      fontSize : '2rem'
+  }
+})
+
 function SideBar() {
+  const classes = useStyles()
   return (
     <div>
       <Grid item xs={12}>
-        <FolderIcon />
+        <FolderIcon style={{fontSize : "100px", color: "#fadc80", padding : '10px'}}/>
       </Grid>
       <Grid item xs={12}>
-        <TuneIcon />
+        <TuneIcon style={{fontSize : "100px", color: "#5f5f5e", padding : '10px'}}/>
       </Grid>
       <Grid item xs={12}>
-        <VisibilityIcon />
+        <VisibilityIcon style={{fontSize : "100px", color: "#5f5f5e", padding : '10px'}}/>
       </Grid>
     </div>
   );
