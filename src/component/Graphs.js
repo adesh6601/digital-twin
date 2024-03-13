02769eb51f0281d686e8d5028c48502f7c06595d
Kunal <kunal.more@cctech.co.in>
2024-03-13 12:06:20 +0530

    ui-almost-completion-kunal

--- src/component/Graphs.js
+++ src/component/Graphs.js
@@ -1,132 +1,127 @@
 import { Grid } from "@mui/material";
 import makeStyles from "@mui/styles/makeStyles";
-import React from "react";
-import { Chart } from "chart.js";
+import * as React from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
 
 const useStyles = makeStyles({
-  graph1: {},
-  graph1: {},
+  graphContainer1: {
+    backgroundColor: "white",
+    marginTop: "20px",
+    marginLeft: "100px",
+    marginRight: "20px",
+    width: "800px",
+    textAlign: "center",
+  },
+  graphContainer2: {
+    backgroundColor: "white",
+    marginTop: "20px",
+    marginLeft: "100px",
+    marginRight: "20px",
+    width: "800px",
+  },
+  selectionDiv1: {
+    display: "flex",
+    marginLeft: "100px",
+    marginTop: "1rem",
+  },
+  selectionDiv2: {
+    display: "flex",
+    marginLeft: "100px",
+    marginTop: "4rem",
+  },
+  resultTypeText: {
+    fontSize: "1.5rem",
+  },
 });
 
 function Graphs() {
   const classes = useStyles();
   const xValues = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
-  function createChart(chartId, datasets) {
-    // const ctx = document.getElementById(chartId).getContext("2d");
-    // return new Chart(ctx, {
-    //   type: "line",
-    //   data: {
-    //     labels: xValues,
-    //     datasets: datasets,
-    //   },
-    //   options: {
-    //     legend: {
-    //       display: false,
-    //     },
-    //     responsive: true,
-    //   },
-    // });
-  }
-
   const datasets = [
-    {
-      label: "Dataset 1",
-      data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478],
-      borderColor: "red",
-      fill: false,
-      hidden: false,
-    },
-    {
-      label: "Dataset 2",
-      data: [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000],
-      borderColor: "green",
-      fill: false,
-      hidden: false,
-    },
-    {
-      label: "Dataset 3",
-      data: [300, 700, 2000, 5000, 6000, 4000, 2000, 1000, 200, 100],
-      borderColor: "blue",
-      fill: false,
-      hidden: false,
-    },
+    [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478],
+    [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000],
+    [300, 700, 2000, 5000, 6000, 4000, 2000, 1000, 200, 100],
   ];
+  const colors = ["red", "green", "blue"];
 
-  const chart1 = createChart("myChart1", datasets);
-  const chart2 = createChart("myChart2", datasets);
+  const [selectedColor1, setSelectedColor1] = React.useState("red");
+  const [selectedColor2, setSelectedColor2] = React.useState("red");
 
-  function updateChart(chart, selectedValue) {
-    if (selectedValue.toLowerCase() === "all") {
-      chart.data.datasets.forEach((dataset) => {
-        dataset.hidden = false;
-      });
-    } else {
-      chart.data.datasets.forEach((dataset) => {
-        if (dataset.borderColor.toLowerCase() === selectedValue.toLowerCase()) {
-          dataset.hidden = false;
-        } else {
-          dataset.hidden = true;
-        }
-      });
-    }
-    chart.update();
-  }
+  const handleChangeColor1 = (event) => {
+    setSelectedColor1(event.target.value);
+  };
+
+  const handleChangeColor2 = (event) => {
+    setSelectedColor2(event.target.value);
+  };
 
   return (
     <div>
-      <Grid item xs={12} style={{ height: "23vh", marginTop: "2vh" }}>
-        <div class="dropdown mt-5" style={{ display: "flex" }}>
-          <span style={{ marginLeft: "100px", fontSize: "2vh" }}>
-            {" "}
-            Result Type :{" "}
-          </span>
+      <Grid>
+        <div className={classes.selectionDiv1}>
+          <span className={classes.resultTypeText}>Result Type :</span>
           <select
+            value={selectedColor1}
+            onChange={handleChangeColor1}
             style={{ textAlign: "left", marginLeft: "2vh", width: "20vh" }}
-            id="graph1Dropdown"
-            onchange="updateChart(chart1, this.value)"
+            id="graphDropdown1"
           >
-            <option value="all">All</option>
-            <option value="red">Red</option>
-            <option value="green">Green</option>
-            <option value="blue">Blue</option>
+            {colors.map((color) => (
+              <option key={color} value={color}>
+                {color.charAt(0).toUpperCase() + color.slice(1)}
+              </option>
+            ))}
           </select>
         </div>
       </Grid>
 
-      <Grid style={{ height: "23vh" }}>
-        <div
-          class="graph-container"
-          style={{ textAlign: "left", marginLeft: "50px" }}
-        >
-          <canvas id="myChart1"></canvas>
+      <Grid>
+        <div className={classes.graphContainer1}>
+          <LineChart
+            xAxis={[{ data: xValues }]}
+            series={[
+              {
+                data: datasets[colors.indexOf(selectedColor1)],
+                color: selectedColor1,
+              },
+            ]}
+            width={800}
+            height={400}
+          />
         </div>
       </Grid>
 
-      <Grid item xs={12} style={{ height: "23vh", marginTop: "2vh" }}>
-        <div class="dropdown mt-5" style={{ display: "flex" }}>
-          <span style={{ marginLeft: "100px", fontSize: "2vh" }}>
-            {" "}
-            Result Type :{" "}
-          </span>
+      <Grid>
+        <div className={classes.selectionDiv2}>
+          <span className={classes.resultTypeText}>Result Type :</span>
           <select
+            value={selectedColor2}
+            onChange={handleChangeColor2}
             style={{ textAlign: "left", marginLeft: "2vh", width: "20vh" }}
-            id="graph1Dropdown"
-            onchange="updateChart(chart2, this.value)"
+            id="graphDropdown2"
           >
-            <option value="all">All</option>
-            <option value="red">Red</option>
-            <option value="green">Green</option>
-            <option value="blue">Blue</option>
+            {colors.map((color) => (
+              <option key={color} value={color}>
+                {color.charAt(0).toUpperCase() + color.slice(1)}
+              </option>
+            ))}
           </select>
         </div>
       </Grid>
 
-      <Grid style={{ height: "23vh" }}>
-        <div
-          class="graph-container"
-          style={{ textAlign: "left", marginLeft: "50px" }}
-        >
-          <canvas id="myChart1"></canvas>
+      <Grid>
+        <div className={classes.graphContainer2}>
+          <LineChart
+            xAxis={[{ data: xValues }]}
+            series={[
+              {
+                data: datasets[colors.indexOf(selectedColor2)],
+                color: selectedColor2,
+              },
+            ]}
+            width={800}
+            height={400}
+          />
         </div>
       </Grid>
     </div>
